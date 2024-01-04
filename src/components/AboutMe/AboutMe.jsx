a16432fbb5d637a5adc9c9903800e6e5b1f0f1f4
oindrilabanerjee <banerjee.oindrila.ob@gmail.com>
2024-01-04 23:53:44 +0530

    Added about me section

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -41,15 +41,15 @@ const AboutMe = () => {
             </motion.div>
             < div className="radial-skills">
                <Skill name={"Backend"} x="0" y="0"/>
-               <Skill name={"Java"} x="-22vw" y="2vw" />
+               <Skill name={"Java"} x="-20vw" y="2vw" />
                <Skill name={"SpringBoot"} x="-25vw" y="-14vw" />
-               <Skill name={"MySQL"} x="0vw" y="-16.5vw" />
-               <Skill name={"React JS"} x="-12vw" y="-7vw" />
-               <Skill name={"Dart"} x="6vw" y="9vw" />
+               <Skill name={"MySQL"} x="0vw" y="-15vw" />
+               <Skill name={"React JS"} x="-10vw" y="-7vw" />
+               <Skill name={"Dart"} x="6vw" y="8vw" />
                <Skill name={"Python"} x="23vw" y="-14vw" />
-               <Skill name={"NodeJS"} x="22vw" y="10vw" />
-               <Skill name={"Git"} x="16vw" y="-2.5vw" />
-               <Skill name={"LLD"} x="-15vw" y="14vw" />
+               <Skill name={"NodeJS"} x="19vw" y="10vw" />
+               <Skill name={"Git"} x="15vw" y="-2.5vw" />
+               <Skill name={"LLD"} x="-10vw" y="14vw" />
             </div>
         </div>
     )
