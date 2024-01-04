632230fde33b7acc9555c6676e0b89b4403d8a62
oindrilabanerjee <banerjee.oindrila.ob@gmail.com>
2024-01-05 00:01:33 +0530

    Added about me section

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -42,14 +42,14 @@ const AboutMe = () => {
             < div className="radial-skills">
                <Skill name={"Backend"} x="0" y="0"/>
                <Skill name={"Java"} x="-20vw" y="2vw" />
-               <Skill name={"SpringBoot"} x="-25vw" y="-14vw" />
+               <Skill name={"SpringBoot"} x="-20vw" y="-14vw" />
                <Skill name={"MySQL"} x="0vw" y="-15vw" />
                <Skill name={"React JS"} x="-10vw" y="-7vw" />
                <Skill name={"Dart"} x="6vw" y="8vw" />
-               <Skill name={"Python"} x="23vw" y="-14vw" />
+               <Skill name={"Python"} x="20vw" y="-14vw" />
                <Skill name={"NodeJS"} x="19vw" y="10vw" />
                <Skill name={"Git"} x="15vw" y="-2.5vw" />
-               <Skill name={"LLD"} x="-10vw" y="14vw" />
+               <Skill name={"LLD"} x="-10vw" y="13.5vw" />
             </div>
         </div>
     )
